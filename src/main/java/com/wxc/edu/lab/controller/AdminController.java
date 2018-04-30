952a3f4798c9ace8230e5b11cc92590f1d9222c5
rubinstein1997 <liuyunlong0908@outlook.com>
2018-04-30 17:06:39 +0800

    add static file

--- src/main/java/com/wxc/edu/lab/controller/AdminController.java
+++ src/main/java/com/wxc/edu/lab/controller/AdminController.java
@@ -5,6 +5,7 @@ import com.wxc.edu.lab.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +21,10 @@ public class AdminController {
         return adminService.selectById(id);
     }
 
-    @RequestMapping("/login",)
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginForm() {
+        return "login";
+    }
 
 
 }
